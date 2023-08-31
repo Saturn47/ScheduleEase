0080f788ba8a63ac054eabce6cc18444992edd08
Rithika Singh <101629832+Saturn47@users.noreply.github.com>
2023-08-31 21:12:30 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://scheduleease-backend-server.onrender.com/';
 
 export const register = async (name, email, password) => {
   const response = await axios.post(`${API_URL}/auth/register`, { name, email, password });
