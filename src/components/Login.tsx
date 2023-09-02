318cf03bcbc6bee8130c00f056b7784cfa5e13a6
Rithika Singh <101629832+Saturn47@users.noreply.github.com>
2023-09-02 22:11:39 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,29 +1,34 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
 import './Login.css';
 
 const Login: React.FC = () => {
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [otp, setOTP] = useState('');
+  const [otpRequested, setOtpRequested] = useState(false); // Track whether OTP is requested
   const navigate = useNavigate();
+  const API_URL = 'https://scheduleease-backend-server.onrender.com';
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
+  const handleRequestOTP = async () => {
     try {
-      const response = await fetch('https://scheduleease-backend-server.onrender.com/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          email,
-          password
-        })
-      });
+      const response = await axios.post(`${API_URL}/request-otp`, { email });
+      if (response.status === 200) {
+        toast.success('OTP sent to your email'); // Show success toast
+        setOtpRequested(true); // OTP requested, show OTP input field
+      } else {
+        toast.error('OTP request failed'); // Show error toast
+      }
+    } catch (error) {
+      toast.error('OTP request error:');
+    }
+  };
 
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(`${API_URL}/login`, { email, otp });
       if (response.status === 200) {
         toast.success('Login successful'); // Show success toast
         navigate('/calendar');
@@ -31,7 +36,7 @@ const Login: React.FC = () => {
         toast.error('Login failed'); // Show error toast
       }
     } catch (error) {
-      console.error('Login error:', error);
+      toast.error('Login error:');
     }
   };
 
@@ -40,18 +45,39 @@ const Login: React.FC = () => {
       <ToastContainer /> {/* Initialize ToastContainer */}
       <h1>Welcome to ScheduleEase!!</h1>
       <h2>Login into your account</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => e.preventDefault()}>
         <div>
           <label htmlFor='email'>Email</label>
-          <input id='email' name='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            id='email'
+            name='email'
+            type='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input id='password' name='password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Login</button>
+        {otpRequested ? (
+          <div>
+            <label htmlFor='otp'>OTP</label>
+            <input
+              id='otp'
+              name='otp'
+              type='text'
+              value={otp}
+              onChange={(e) => setOTP(e.target.value)}
+            />
+          </div>
+        ) : null}
+        {otpRequested ? (
+          <button type='button' onClick={handleLogin}>
+            Login with OTP
+          </button>
+        ) : (
+          <button type='button' onClick={handleRequestOTP}>
+            Request OTP
+          </button>
+        )}
       </form>
-      <p>Don't have an account? <Link to="/register">Register here</Link></p>
     </div>
   );
 };
